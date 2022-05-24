7b95ca9e20eb4126331de407b8780d214e1c704c
bohecola <bohecola@outlook.com>
2022-05-24 19:00:40 +0800

    feat: finished project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const {
   initFolders,
   initFiles,
-  // initWatchers
+  initWatchers
 } = require('./init');
 
 const {
@@ -38,7 +38,7 @@ class ViteDocCreator {
     // 初始化项目文件
     initFiles(this.options);
     // 初始化监听html与markdown文件及文件夹变化
-    // initWatchers(this.options);
+    initWatchers(this.options);
 
     mdToHtml('guide.md')
   }
